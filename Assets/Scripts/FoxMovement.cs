7ced4d8d7fb3f49c214cb61ee5a415540f5ae614
Jocelyn Chen <jocelynchen1246@berkeley.edu>
2020-03-09 00:26:06 -0700

    sune movement
    edited sune prefab/sprite to add movement for side to side and jumping

---
+++ Assets/Scripts/FoxMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoxMovement : MonoBehaviour
+{
+    public FoxController controller;
+    public float runSpeed = 20f;
+
+    float horizontalMove = 0f;
+    bool jump = false;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
+    }
+}
