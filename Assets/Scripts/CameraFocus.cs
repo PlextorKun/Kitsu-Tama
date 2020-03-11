2b4f3a6545a76af1c2c86b9b546f38190bea816e
Sambodh Mitra <sambodh.mitra@berkeley.edu>
2020-03-10 17:09:00 -0700

    Wrote camera script and built layers/tags

---
+++ Assets/Scripts/CameraFocus.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFocus : MonoBehaviour
+{
+    private Transform playerTransform;
+
+    void Start() {
+          playerTransform = GameObject.FindGameObjectWithTag("Sune Tama").transform;
+
+    }
+
+    // Update is called once per frame
+    void LateUpdate() {
+
+          Vector3 temp = transform.position;
+          temp.x = playerTransform.position.x;
+          transform.position = temp;
+
+    }
+}
---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public List<Transform> targets;
+
+    public Vector3 offset;
+    private void LateUpdate()
+    {
+        if (targets.Count == 1)
+        {
+            return;
+        }
+
+        Vector3 centerPoint = GetCenterPoint();
+
+        Vector3 newPosition = centerPoint + offset;
+
+        transform.position = newPosition;
+    }
+
+    Vector3 GetCenterPoint()
+    {
+        if (targets.Count == 1)
+        {
+            return targets[0].position;
+        }
+
+        var bounds = new Bounds(targets[0].position, Vector3.zero);
+        for (int index = 0; index < targets.Count; index++)
+        {
+            bounds.Encapsulate(targets[index].position);
+        }
+        return bounds.center;
+    }
+}
