3fb12a8582133a79c268f219027833ade42250b1
Jocelyn Chen <jocelynchen1246@berkeley.edu>
2020-03-17 18:43:44 -0700

    updated attack
    added attack rate and enemy to test attack

--- Assets/Scripts/FoxCombat.cs
+++ Assets/Scripts/FoxCombat.cs
@@ -7,13 +7,21 @@ public class FoxCombat : MonoBehaviour
     public Transform attackPoint;
     public float attackRange = 0.5f;
     public LayerMask enemyLayers;
+
+    public float attackRate = 2f;
+    float nextAttackTime = 0f;
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.RightShift))
+        if (Time.time >= nextAttackTime)
         {
-            Attack();
+            if (Input.GetKeyDown(KeyCode.RightShift))
+            {
+                Attack();
+                nextAttackTime = Time.time + 1f / attackRate;
+            }
         }
+        
     }
 
     void Attack()
