76079e08da5600219970946871ec3a2efbe603f5
Brandon Cho <chobrandon@yahoo.com>
2020-03-17 18:22:59 -0700

    added enemy and enemymovement script

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    private Vector3 original;
+    private Vector3 other;
+    bool moving;
+    bool move_right;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        original = transform.position;
+        other = transform.position + new Vector3(3, 0, 0);
+        moving = false;
+        move_right = true;
+        StartCoroutine(EnemyMoveRight());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!moving && move_right)
+        {
+            StartCoroutine(EnemyMoveRight());
+        }
+        if (!moving && !move_right)
+        {
+            StartCoroutine(EnemyMoveLeft());
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(this.gameObject);
+    }
+
+        IEnumerator EnemyMoveRight()
+    {
+        moving = true;
+        float elapsedTime = 0;
+        float totalTransitionTime = 2;
+        while (transform.position != other)
+        {
+            transform.position = Vector3.Lerp(original, other, elapsedTime / totalTransitionTime);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        move_right = false;
+        moving = false;
+    }
+
+    IEnumerator EnemyMoveLeft()
+    {
+        moving = true;
+        float elapsedTime = 0;
+        float totalTransitionTime = 2;
+        while (transform.position != original)
+        {
+            transform.position = Vector3.Lerp(other, original, elapsedTime / totalTransitionTime);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        move_right = true;
+        moving = false;
+
+    }
+}
