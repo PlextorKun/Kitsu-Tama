fcc854045d7070d5d36c271717eb8970d623a636
Jocelyn Chen <jocelynchen1246@berkeley.edu>
2020-04-07 02:00:07 -0700

    working wall and button obstacle

---
+++ Assets/Scripts/Obstacle1Button.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle1Button : MonoBehaviour
+{
+    public Color activatedColor;
+    public Color inactivatedColor;
+
+    public GameObject wall;
+
+    private bool activated = true;
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (activated)
+        {
+            gameObject.GetComponent<SpriteRenderer>().color = inactivatedColor;
+            wall.GetComponent<Obstacle1Wall>().move();
+            activated = false;
+        }
+        
+
+    }
+
+    public void activateButton()
+    {
+        gameObject.GetComponent<SpriteRenderer>().color = activatedColor;
+        activated = true;
+    }
+
+}
---
+++ Assets/Scripts/Obstacle1Wall.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle1Wall : MonoBehaviour
+{
+    public Color movingColor;
+    public Color stationaryColor;
+    public GameObject button;
+
+    private bool inOriginalPos = true;
+
+    public void move()
+    {
+        gameObject.GetComponent<SpriteRenderer>().color = movingColor;
+        if (inOriginalPos)
+        {
+            StartCoroutine(moveD());
+        } else
+        {
+            StartCoroutine(moveU());
+        }
+        inOriginalPos = !inOriginalPos;
+    }
+
+    private void finishMoving()
+    {
+        gameObject.GetComponent<SpriteRenderer>().color = stationaryColor;
+        button.GetComponent<Obstacle1Button>().activateButton();
+    }
+
+    IEnumerator moveD()
+    {
+        float elapsedTime = 0;
+        float totalTransitionTime = 2;
+        Vector3 original = transform.position;
+        Vector3 other = new Vector3(transform.position.x, transform.position.y - gameObject.GetComponent<BoxCollider2D>().bounds.size.y, transform.position.z);
+        while (transform.position != other)
+        {
+            transform.position = Vector3.Lerp(original, other, elapsedTime / totalTransitionTime);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        finishMoving();
+    }
+
+    IEnumerator moveU()
+    {
+        float elapsedTime = 0;
+        float totalTransitionTime = 2;
+        Vector3 original = transform.position;
+        Vector3 other = new Vector3(transform.position.x, transform.position.y + gameObject.GetComponent<BoxCollider2D>().bounds.size.y, transform.position.z);
+        while (transform.position != other)
+        {
+            transform.position = Vector3.Lerp(original, other, elapsedTime / totalTransitionTime);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        finishMoving();
+    }
+}
