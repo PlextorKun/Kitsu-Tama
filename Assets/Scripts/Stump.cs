04f4dbd8c5d255300442d10c44c35e32ff391e58
Sambodh Mitra <sambodh.mitra@berkeley.edu>
2020-04-13 11:26:10 -0700

    Stumps die from Egg

---
+++ Assets/Scripts/Stump.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stump : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.CompareTag("Egg"))
+        {
+            Die();
+        }
+    }
+
+    public void Die()
+    {
+        Destroy(this.gameObject);
+    }
+}
